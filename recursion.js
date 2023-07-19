99d7fd9337d1eb9ae5ccfddd482289f8d3ef311e
Mohamed Reda <mohamed_aboreda_2020@yahoo.com>
2023-07-19 23:45:24 +0300

    test the functionality

--- recursion.js
+++ recursion.js
@@ -1,10 +1,12 @@
 function recursion(x) {
     if (x == 0) {
-        console.log("done!")
+        console.log("done!") //#2
         return
     } else {
         console.log(x, )
-        recursion(x - 1)
+        recursion(x - 1) //the recursion being recall this untill finesh //#1
+
+        console.log("foo") //#3
     }
 }
 recursion(4)
